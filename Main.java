19542a0aa9ec135edb11fcb576cfcf0c5d4e087c
Marcos Manzanares <marcos.manzanares@patelhs.org>
2020-11-10 15:42:45 +0000

    I declared my int seatnumbers array and I've started to declare my student name array but I ran out of time in class so I had to leave it like it is :(

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+  System.out.println("Hello and welcome everyone! I'm Marcos and I will be your homeroom teacher for the year. Come in, I will assigning you to your seats in just a moment.");
+
+  int seatNumbers[] = {1,2,3,4,5,6,7,8,9,10,12,13,14,15};
+
+  String students[] = {"Zane", "William", "", "", "", "", "", "", "", "", "", "", "","", "", "",};
+
+
   }
 }
