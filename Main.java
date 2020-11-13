1fc64b5b825aea633194cecee4c2932edd6014b5
Marcos Manzanares <marcos.manzanares@patelhs.org>
2020-11-13 22:19:04 +0000

    I moved my arrays and completed my scanners

--- Main.java
+++ Main.java
@@ -6,15 +6,63 @@ class Main {
 
   System.out.println("Hello and welcome everyone! I'm Marcos and I will be your homeroom teacher for the year. Come in, I will assigning you to your seats in just a moment.");
 
-  int seatNumbers[]= {1,2,3,4,5,6,7,8,9,10,12,13,14,15};
+Scanner scan = new Scanner (System.in);{
 
-  String students[] = {};
+System.out.println("Who is the 1st student on your list?");
+String student1 = scan.next();
 
+System.out.println("Who is the 2nd student on your list?");
+String student2 = scan.next();
+
+System.out.println("Who is the 3rd student on your list?");
+String student3 = scan.next();
+
+System.out.println("Who is the 4th student on your list?");
+String student4 = scan.next();
+
+System.out.println("Who is the 5th student on your list?");
+String student5 = scan.next();
+
+System.out.println("Who is the 6th student on your list?");
+String student6 = scan.next();
+
+System.out.println("Who is the 7th student on your list?");
+String student7 = scan.next();
+
+System.out.println("Who is the 8th student on your list?");
+String student8 = scan.next();
+
+System.out.println("Who is the 9th student on your list?");
+String student9 = scan.next();
+
+System.out.println("Who is the 10th student on your list?");
+String student10 = scan.next();
+
+System.out.println("Who is the 11th student on your list?");
+String student11 = scan.next();
+
+System.out.println("Who is the 12th student on your list?");
+String student12 = scan.next();
+
+System.out.println("Who is the 13th student on your list?");
+String student13 = scan.next();
+
+System.out.println("Who is the 14th student on your list?");
+String student14 = scan.next();
+
+System.out.println("Who is the 15th student on your list?");
+String student15 = scan.next();
+
+String students[] = {student1, student2, student3, student4, student5, student6, student7, student8, student9, student10, student11, student12, student13, student14, student15};
+ 
    for (int i = 0; i < students.length; i++)
      {
          System.out.println(students[i] );
      }
 
-
+  int seatNumbers[]= {1,2,3,4,5,6,7,8,9,10,12,13,14,15};
+    
+    
+    }
   }
 }
