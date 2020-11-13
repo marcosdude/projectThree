fe7150f6010d1ef32b02f2e9b27128f211f0045a
Marcos Manzanares <marcos.manzanares@patelhs.org>
2020-11-13 17:44:06 +0000

    I finished my names array and made the print statement for it

--- Main.java
+++ Main.java
@@ -4,9 +4,14 @@ class Main {
 
   System.out.println("Hello and welcome everyone! I'm Marcos and I will be your homeroom teacher for the year. Come in, I will assigning you to your seats in just a moment.");
 
-  int seatNumbers[] = {1,2,3,4,5,6,7,8,9,10,12,13,14,15};
+  int seatNumbers[]= {1,2,3,4,5,6,7,8,9,10,12,13,14,15};
 
-  String students[] = {"Zane", "William", "", "", "", "", "", "", "", "", "", "", "","", "", "",};
+  String students[] = {"Zane ","William ","Jake ","Ethan ","Blake ","Ehren","Ryan ","Elijah ","Christian ","Richard ","Ana ","Alexis ","Riley ","Lawren ","Emma"};
+
+   for (int i = 0; i < students.length; i++)
+     {
+         System.out.println(students[i] );
+     }
 
 
   }
